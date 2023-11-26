59133ad470a705a920187634d85689d75585257d
Murathan <murathanbakti@gmail.com>
2023-11-26 19:45:35 +0300

    Post Routing update

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../models/post.js");
+const Post = require("../models/Post");
 
 router.get("/", (req, res) => {
     res.send("We are one Post");
